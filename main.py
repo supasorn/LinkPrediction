8b527a3c7fa8a41171e44cd73f570008ea5540c4
supasorn <supasorn@gmail.com>
2015-05-10 15:29:47 -0700

    auto commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from __future__ import division
 import scipy as sp
 import numpy as np
+from scipy import io
 
 def main():
-    print "yes"
+    f = io.mmread("data/netflix_mm")
+    print f
 
 if __name__ == "__main__":
     main()
