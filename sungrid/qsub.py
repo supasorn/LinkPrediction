52654e4ddb33511f4e427ba8f8d9af40a5837028
supasorn <supasorn@gmail.com>
2015-05-11 16:44:24 -0700

    auto commit

---
+++ sungrid/qsub.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+if len(sys.argv) == 2:
+    f = open(sys.argv[1], "r")
+    num = 0
+    
+    for l in f:
+        fo = open("sh/%04d.txt" % (num), "w")
+        fo.write("#!/usr/bin/env sh\n")
+        fo.write(l)
+        fo.close()
+        num += 1
+
+    for i in range(num):
+        os.system("qsub -q notcuda.q sh/%04d.sh" % i)
