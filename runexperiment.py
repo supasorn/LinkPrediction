aa0e702ccf91ca7aa19ee855a33a61b695bf9e9b
Supasorn Suwajanakorn <supasorn@ristretto.cs.washington.edu>
2015-05-11 17:28:10 -0700

    auto commit

---
+++ runexperiment.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+
+if not os.path.exists("results"):
+    os.mkdir("results")
+
+    
+lambs = np.linspace(0, 0.2, 21)
+fo = open("sungrid/cmd1.txt", "w")
+count = 0
+for lam in lambs:
+    fo.write("cd /projects/grail/supasorn/LinkPrediction/; ")
+    fo.write("python2.7 parallelSGD.py --train=ratings_debug_train.mtx --test=ratings_debug_test.mtx --lamb=%f --rmseint=1 --maxit=50 > results/%04d.txt\n" % (lam, count))
+    count += 1
+    print lam
+fo.close()
+
+os.system("cd /projects/grail/supasorn/LinkPrediction/sungrid/; python qsub.py cmd1.txt")
--- sungrid/qsub.py
+++ sungrid/qsub.py
@@ -6,12 +6,13 @@ if len(sys.argv) == 2:
     num = 0
     
     for l in f:
-        fo = open("sh/%04d.sh" % (num), "w")
+        fo = open("sh/s%04d.sh" % (num), "w")
         fo.write("#!/usr/bin/env sh\n")
         fo.write(l)
         fo.close()
         num += 1
 
     for i in range(num):
-        print "qsub -q notcuda.q sh/%04d.sh" % i
-        os.system("qsub -q notcuda.q sh/%04d.sh" % i)
+        st = "qsub -q all.q sh/s%04d.sh" % i
+        print st
+        os.system(st)
