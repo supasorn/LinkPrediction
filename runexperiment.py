e3a14ee3df517598e0691140967afd0d00302f4d
supasorn <supasorn@gmail.com>
2015-05-13 22:14:39 -0700

    auto commit

--- runexperiment.py
+++ runexperiment.py
@@ -10,7 +10,7 @@ fo = open("sungrid/cmd1.txt", "w")
 count = 0
 for lam in lambs:
     fo.write("cd /projects/grail/supasorn/LinkPrediction/; ")
-    fo.write("python2.7 parallelSGD.py --train=ratings_debug_train.mtx --test=ratings_debug_test.mtx --lamb=%f --rmseint=1 --maxit=50 > results/%04d.txt\n" % (lam, count))
+    fo.write("python2.7 parallelSGD.py --train=ratings_debug_train.mtx --test=ratings_debug_test.mtx --lamb=%f --rmseint=1 --maxit=300 > results/%04d.txt\n" % (lam, count))
     count += 1
     print lam
 fo.close()
