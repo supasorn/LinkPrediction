928ce29cf7b203d48f57e44d2b287ac98f15abae
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-06-05 11:29:18 -0700

    change name for 3-way split

--- slicedata.py
+++ slicedata.py
@@ -6,5 +6,5 @@ from lib_transform import Transformer, sample_split  # , get_debug
 T = Transformer()
 
 train_ids, validate_ids, test_ids = sample_split(T.nr, n_validate=T.nr/5)
-T.save_ratings_splits_mtx(train_ids, validate_ids, test_ids, 'ratings3normal')
-T.save_ratings_splits_sf(train_ids, validate_ids, test_ids, 'ratings3normal')
+T.save_ratings_splits_mtx(train_ids, validate_ids, test_ids, 'full_ratings')
+T.save_ratings_splits_sf(train_ids, validate_ids, test_ids, 'full_ratings')
